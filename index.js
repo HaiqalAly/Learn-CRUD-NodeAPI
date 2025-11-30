fb45224164cb21c7e578a4cbacfc2abcb83d5a21
Haiqal <haiqalaly@gmail.com>
2025-11-30 12:55:38 +0700

    feat: implement product controller with CRUD operations and update routing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
 const port = 3000;
 
 require("dotenv").config();
@@ -11,74 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('api/products', productRoute);
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World! This is my first API");
-});
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/products/:id", async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.status(201).json(product);
-    } else {
-      res.status(404).json({ message: "Product Not Found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Update a product
-app.put("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product Not Found" });
-    }
-
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Delete a product
-app.delete("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product Not Found" });
-    }
-
-    res.status(200).json({ message: "Product Deleted Successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 mongoose
